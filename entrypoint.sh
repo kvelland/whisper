e046e5a21bf60186a4209d9ed4de53c58797e2dd
Espen Kvelland <kvelland@wakandi.com>
2026-05-03 12:03:34 +0300

    Add PIN gate with keypad UI and server-injected code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 set -e
 
-# Inject OPENROUTER_API_KEY into the HTML at container startup
+# Inject env vars into the HTML at container startup
 SAFE_KEY=$(echo "${OPENROUTER_API_KEY:-}" | sed 's/[&/\]/\\&/g')
-sed "s|__OPENROUTER_API_KEY__|${SAFE_KEY}|g" \
+SAFE_PIN=$(echo "${SITE_PIN:-}" | sed 's/[&/\]/\\&/g')
+
+sed \
+  -e "s|__OPENROUTER_API_KEY__|${SAFE_KEY}|g" \
+  -e "s|__SITE_PIN__|${SAFE_PIN}|g" \
   /usr/share/nginx/html/index.html.template \
   > /usr/share/nginx/html/index.html
 
